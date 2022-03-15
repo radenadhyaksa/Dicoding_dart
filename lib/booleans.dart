0c7507ead76e7e1e166a6d1383661af89a48f926
radenadhyaksa <raksa69@gmail.com>
2022-03-15 20:38:26 +0700

    add example booleans.dart

---
+++ lib/booleans.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  ///rumus boolean
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
+  var notTrue = !true;
+  bool notFalse = !false;
+
+  if (!true) {
+    print("It's true");
+  } else {
+    print("It's False");
+  }
+}
