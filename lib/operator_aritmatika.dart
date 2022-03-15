7fa2c67f0d34808642c49bce604462f93bc8374d
radenadhyaksa <raksa69@gmail.com>
2022-03-15 20:42:24 +0700

    update example operator_aritmatika.dart

--- lib/operator_aritmatika.dart
+++ lib/operator_aritmatika.dart
@@ -8,4 +8,12 @@ void main(List<String> args) {
 Output :
   17
  */
+
+  /// rumus aritmatika
+  print(5 + 2); // int add = 7
+  print(5 - 2); // int subtract = 3
+  print(5 * 2); // int multiply = 10
+  print(5 / 2); // double divide = 2.5
+  print(5 ~/ 2); // int intDivide = 2
+  print(5 % 2); // int modulo = 1
 }
