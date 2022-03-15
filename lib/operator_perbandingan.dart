726adc2d13a162f7623600bc1922e4bfd0a1a70c
radenadhyaksa <raksa69@gmail.com>
2022-03-15 20:49:39 +0700

    update example operator_perbandingan.dart

---
+++ lib/operator_perbandingan.dart
@@ -0,0 +1,20 @@
+void main() {
+  /**
+   * ==	Sama dengan
+   * !=	Tidak sama dengan
+   * >	Lebih dari
+   * <	Kurang dari
+   * >=	Lebih dari sama dengan
+   * <=	Kurang dari sama dengan */
+
+  if (2 <= 3) {
+    print('Ya, 2 kurang dari sama dengan 3');
+  } else {
+    print('Anda salah');
+  }
+
+/*
+Output:
+  Ya, 2 kurang dari sama dengan 3
+ */
+}
