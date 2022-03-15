57840e7536278c3f2d9f34594432b5ffe9e53156
radenadhyaksa <raksa69@gmail.com>
2022-03-15 20:19:56 +0700

    add comment.dart

---
+++ lib/comment.dart
@@ -0,0 +1,8 @@
+ 
+/// Fungsi [main] akan menampilkan 2 output
+/// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada library [Belajar_Dart]
+void main() {
+  // Mencetak Hello Dart! Dart is great. pada konsol
+  print('Hello Dart! Dart is great.');
+  // Testing documentation comment with [].
+}
