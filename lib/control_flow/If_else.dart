3d2d79c7eef10d578bc4e2831e16e73781cbcf1f
radenadhyaksa <raksa69@gmail.com>
2022-03-16 17:18:18 +0700

    update if & else

--- lib/control_flow/If_else.dart
+++ lib/control_flow/If_else.dart
@@ -8,4 +8,40 @@ void main() {
   } else {
     print("Sorry, we’ve closed");
   }
+  /**
+   * versi simple
+   * Fitur menarik lain dari Dart adalah conditional expressions. 
+   * Dengan ini kita bisa menuliskan if-else statement hanya dalam satu baris:
+   * /
+  //var shopStatus = now > openHours ? "Hello, we're open" : "Sorry, we've closed";
+  //print(shopStatus);
+
+  //=================================================
+
+  /*
+  dapat mengecek beberapa kondisi 
+  sekaligus dengan menggabungkan else dan if. 
+  Contohnya seperti program konversi nilai berikut:
+   */
+
+  // void main() {
+  //   stdout.write('Inputkan nilai Anda (1-100) : ');
+  //   var score = num.parse(stdin.readLineSync()!);
+
+  //   print('Nilai Anda: ${calculateScore(score)}');
+  // }
+
+  // String calculateScore(num score) {
+  //   if (score > 90) {
+  //     return 'A';
+  //   } else if (score > 80) {
+  //     return 'B';
+  //   } else if (score > 70) {
+  //     return 'C';
+  //   } else if (score > 60) {
+  //     return 'D';
+  //   } else {
+  //     return 'E';
+  //   }
+  // }
 }
