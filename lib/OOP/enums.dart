647be639d070fe9b8ea587301a74297da4474a9f
radenadhyaksa <raksa69@gmail.com>
2022-03-17 20:57:39 +0700

    update example simple OOP - enums

--- lib/OOP/enums.dart
+++ lib/OOP/enums.dart
@@ -12,9 +12,35 @@ void main() {
   print(Rainbow.values);
   print(Rainbow.blue);
   print(Rainbow.orange.index);
- 
-/// output : 
+
+  /// output :
 // [Rainbow.red, Rainbow.orange, Rainbow.yellow, Rainbow.green, Rainbow.blue, Rainbow.indigo, Rainbow.violet]
 // Rainbow.blue
 // 1
+///================================
+
+
+/**
+ * bisa menggunakan enum ke dalam switch statements. 
+ * Namun kita perlu menangani semua kemungkinan nilai enum yang Ada.
+ */
+  var taskStatus = Status.In_Progress;
+
+  switch (taskStatus) {
+    case Status.Todo:
+      print('Task is still in To do');
+      break;
+    case Status.In_Progress:
+      print('Task is in progress');
+      break;
+    case Status.In_Review:
+      print('Task is currently under review');
+      break;
+    case Status.Done:
+      print('Task is done');
+      break;
+  }
+
+  /// output :
+  /// Task is in progress
 }
