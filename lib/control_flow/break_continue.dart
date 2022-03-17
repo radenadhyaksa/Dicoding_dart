fcdaf0fcdb7639baffe98b861c2fb2f64c69d866
radenadhyaksa <raksa69@gmail.com>
2022-03-17 11:01:11 +0700

    update example break & continue

--- lib/control_flow/break_continue.dart
+++ lib/control_flow/break_continue.dart
@@ -15,3 +15,24 @@ void main() {
     }
   }
 }
+
+///ini contoh perulangan continue
+void breakContinue() {
+  for (int i = 1; i <= 10; i++) {
+    if (i % 3 == 0) {
+      continue;
+    }
+    print(i);
+  }
+}
+
+/*
+output :
+  1
+  2
+  4
+  5
+  7
+  8
+  10
+*/
