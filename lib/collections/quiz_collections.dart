150108a1c32aa512e56f283694ec2dae3d0a2a16
radenadhyaksa <raksa69@gmail.com>
2022-03-17 12:40:07 +0700

    add collection - quiz

---
+++ lib/collections/quiz_collections.dart
@@ -0,0 +1,12 @@
+void main() {
+  var capital = {
+    'Jakarta': 'Indonesia', 
+    'London': 'England', 
+    'Tokyo': 'Japan'};
+
+  capital['New Delhi'] = 'India';
+
+  print(capital['Tokyo']);
+
+  //output : Japan
+}
