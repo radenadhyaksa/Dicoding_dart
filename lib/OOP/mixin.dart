4ded288a462e762e21e7c44ac106646ff1c01e39
radenadhyaksa <raksa69@gmail.com>
2022-03-17 21:07:35 +0700

    update example simple OOP - mixins

--- lib/OOP/mixin.dart
+++ lib/OOP/mixin.dart
@@ -15,3 +15,19 @@ mixin Swimmable {
     print("I'm swimming");
   }
 }
+
+class Cat with Walkable {}
+
+class Duck with Walkable, Flyable, Swimmable {}
+
+///Dengan mixin ini memungkinkan objek cat untuk memanggil metode walk().
+///Sementara objek duck bisa memanggil metode walk(), fly(), dan swim().
+void main() {
+  var donald = Duck();
+  var garfield = Cat();
+
+  garfield.walk();
+
+  donald.walk();
+  donald.swim();
+}
