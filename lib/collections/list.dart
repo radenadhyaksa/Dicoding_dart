5c0a284096b88b1efab7b16f96ced2c567cfea4f
radenadhyaksa <raksa69@gmail.com>
2022-03-17 11:55:52 +0700

    update collection -file list

--- lib/collections/list.dart
+++ lib/collections/list.dart
@@ -1,5 +1,4 @@
 void main(List<String> args) {
-
   /**
    * Untuk menampilkan seluruh item dari list kita bisa memanfaatkan looping. 
    * Contohnya perhatikan kode berikut:
@@ -8,7 +7,11 @@ void main(List<String> args) {
   var stringList = ['Hello', 'Dicoding', 'Dart']; //string list
   List dynamicList = [1, 'Dicoding', true]; //dynamic list
 
-  for (int i = 0; i < dynamicList.length; i++) {
-    print(dynamicList[i]);
+  // stringList.add('Flutter'); ///menambahkan data ke dalam list (posisi di akhir list)
+  // stringList.insert(0, 'Programming'); ///menambahkan data namun tidak di akhir List
+  // stringList[1] = 'Application'; ///Untuk mengubah nilai di dalam list (sesuai indeks yang ditentukan)
+
+  for (int i = 0; i < stringList.length; i++) {
+    print(stringList[i]);
   }
 }
