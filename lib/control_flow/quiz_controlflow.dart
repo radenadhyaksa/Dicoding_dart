818f723c26e2f61bbda678d49fa08373be9e081a
radenadhyaksa <raksa69@gmail.com>
2022-03-17 11:17:52 +0700

    add quiz control flow

---
+++ lib/control_flow/quiz_controlflow.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var x = 11;
+
+  if (x * 2 + 1 < 23 && x % 2 == 1) {
+    print('x');
+  } else if (x != 0) {
+    print('y');
+  } else {
+    print('z');
+  }
+}
