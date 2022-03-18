fad0db631e2dc2eabed0810c15ca58c23916f154
radenadhyaksa <raksa69@gmail.com>
2022-03-18 15:03:05 +0700

    update dart future - async_await.

--- lib/dart_future/async_await.dart
+++ lib/dart_future/async_await.dart
@@ -5,8 +5,35 @@ Dart memiliki keyword async dan await
 yang merupakan sebuah alternatif supaya 
 bisa menuliskan proses asynchronous layaknya proses synchronous. */
 
-void main() {
+// void main() async {
+//   print('Getting your order...');
+//   var order = await getOrder();
+//   print('You order: $order');
+// }
+
+/*
+  Output :
+  Getting your order...
+  You order: Coffee Boba
+*/
+
+//====================
+///menangani ketika terjadi eror?
+///Caranya cukup sederhana yaitu dengan memanfaatkan try-catch:
+
+void main() async {
   print('Getting your order...');
-  var order = getOrder();
-  print('You order: $order');
+  try {
+    var order = await getOrder();
+    print('You order: $order');
+  } catch (error) {
+    print('Sorry. $error');
+  } finally {
+    print('Thank you');
+  }
 }
+
+// output:
+// Getting your order...
+// Sorry. Our stock is not enough.
+// Thank you
