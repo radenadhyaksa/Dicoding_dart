6ec282ae2bcca7c958fc3597c2ff96c38bce3bbf
radenadhyaksa <raksa69@gmail.com>
2022-03-18 14:48:21 +0700

    update dart future - asynchronous future.

--- lib/dart_future/future.dart
+++ lib/dart_future/future.dart
@@ -4,10 +4,53 @@ ada tiga kondisi yang mungkin terjadi, antara lain :
 2. paket dibuka lalu berisi barang sesuai pesanan (completed with data), 
 3. dan paket dibuka namun terjadi kesalahan atau tidak sesuai (completed with error). */
 
+// void main() {
+//   final myFuture = Future(() {
+//     print('Creating the future');
+//     return 12;
+//   });
+//   print('main() done');
+// }
+
+/**
+ seluruh fungsi main akan dieksekusi sebelum fungsi yang ada di dalam Future(). 
+ Ini disebabkan karena future masih berstatus uncompleted. 
+ output :
+main() done
+Creating the future */
+
+//=============================================
+
+///completed with error.
+
 void main() {
-  final myFuture = Future(() {
-    print('Creating the future');
-    return 12;
+  getOrder().then((value) {
+    print('You order: $value');
+  }).catchError((error) {
+    print('Sorry. $error');
+  }).whenComplete(() { //Method ini akan dijalankan ketika suatu fungsi Future selesai dijalankan, tak peduli apakah menghasilkan nilai atau eror.
+    print('Thank you');
   });
-  print('main() done');
+  print('Getting your order...');
 }
+
+Future<String> getOrder() {
+  return Future.delayed(Duration(seconds: 3), () {
+    var isStockAvailable = false;
+    if (isStockAvailable) {
+      return 'Coffee Boba';
+    } else {
+      throw 'Our stock is not enough.';
+    }
+  });
+}
+
+///output :
+  // Getting your order...
+  // Sorry. Our stock is not enough.
+
+
+  /*
+  - Fungsi getOrder() yang berisi Future yang masih uncompleted.
+  - Method then() yang menangani kondisi completed with data.
+  - Method catchError() yang menangani kondisi completed with error. */
